7a0281916f50f4169282840e5ba4cbe165e7b753
redsnowc <nuanyang.44@gmail.com>
2019-12-13 20:20:19 +0800

    plus

--- Basics/assets/scripts/app.js
+++ Basics/assets/scripts/app.js
@@ -1,1 +1,15 @@
-alert('its works!')
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add() {
+    console.log(userInput.value);
+    currentResult = currentResult + Number(userInput.value);
+    outputResult(currentResult, '');
+}
+
+addBtn.addEventListener('click', add)
+
+let calculationDescription = `${defaultResult} + 10`
+let errorMessage = 'An error \noccurred!'
+
+// outputResult(currentResult, errorMessage);
